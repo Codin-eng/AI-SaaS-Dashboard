4b2872229cd30a705a33431c8a7ceffd88950924
codin-eng <bratha500@gmail.com>
2026-05-28 13:00:02 +0300

    remove backend

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-app.use((req, res, next) => {
-  console.log("GLOBAL HIT:", req.method, req.url);
-  next();
-});
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running...");
-});
